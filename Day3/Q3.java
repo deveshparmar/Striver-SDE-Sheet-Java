2037094fd24a7fb5ab2247db49bd17695af92e12
Devesh Parmar <81907545+deveshparmar@users.noreply.github.com>
2022-07-06 13:36:24 -0400

    Q3: Majority Element

---
+++ Day3/Q3.java
@@ -0,0 +1,21 @@
+package Day3;
+
+public class Q3 {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int element = 0;
+
+        for(int i=0;i<nums.length;i++){
+            if(count==0){
+                element = nums[i];
+            }
+            if(element == nums[i]){
+                count++;
+            }else if(element!=nums[i]){
+                count--;
+            }
+        }
+
+        return element;
+    }
+}
